5a402de4a1df043f3e3d0d66bafd46fecb487b66
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-24 06:02:59 +0300

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,4 +20,4 @@ class BasicCache(BaseCaching):
         """ Get an item by key """
         if key is None or key not in self.cache_data.keys():
             return None
-        return self.cache_data[key]
+        return self.cache_data.get(key)
