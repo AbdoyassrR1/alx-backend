21255b79b423a525d5c41917739fede4b98bd6cf
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-24 21:14:52 +0300

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python3
-""" BaseCaching module """
+#!/usr/bin/python3
+"""Basic Cache implementation Class
+"""
 BaseCaching = __import__('BaseCaching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache """
-    def __init__(self):
-        """Initiliazef"""
-        super().__init__()
+    """
+    A basic cache implementaion class
 
+    Attributes:
+        MAX_ITEMS: number of items that can be store in the cache
+    """
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is None or item is None:
-            pass
-        else:
-            self.cache_data[key] = item
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data.update({key: item})
 
     def get(self, key):
-        """ Get an item by key """
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data[key]
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
