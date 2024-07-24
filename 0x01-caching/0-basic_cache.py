0fe646ecb51c2b9f4c21b082b7f5d413e1ac996e
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-24 06:00:25 +0300

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,8 +12,9 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache """
         if key is None or item is None:
-            return
-        self.cache_data[key] = item
+            pass
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
