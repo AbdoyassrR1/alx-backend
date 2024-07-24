c0068f520204fc256bdf5b541408464480c9042e
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-24 05:49:17 +0300

    basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+""" BaseCaching module """
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+    - constants of your caching system
+    - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError(
+            "put must be implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError(
+            "get must be implemented in your cache class")
+
+
+class BasicCashe(BaseCaching):
+    """ BasicCache """
+    def __init__(self):
+        """Initiliazef"""
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or not key:
+            return None
+        return self.cache_data[key]
