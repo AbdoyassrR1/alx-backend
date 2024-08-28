c075b145dcaea68422094755ee293dac896f5be1
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-08-28 22:21:04 +0300

    set and get

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+import { createClient, print } from "redis";
+
+const client = createClient();
+
+client.on("connect", () => console.log("Redis client connected to the server"));
+client.on("error", err => console.log("Redis client not connected to the server: ", err));
+
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, print)
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, value) => {
+    console.log(value);
+  });
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
